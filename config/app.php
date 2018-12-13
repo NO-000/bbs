f13e6e0499af27a0a3ecce521202aaf1aa20fc54
YYYY <11111@YYYY.com>
2018-12-13 10:46:31 +0800

    配置百度翻译

--- config/app.php
+++ config/app.php
@@ -4,6 +4,12 @@ return [
 
 
 
+    'baidu_translate' => [
+        'appid' => env('BAIDU_TRANSLATE_APPID'),
+        'key'   => env('BAIDU_TRANSLATE_KEY'),
+    ],
+
+
     /*
     |--------------------------------------------------------------------------
     | Application Name
